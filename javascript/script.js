936d7a43bf7ea091ef3fa5059287ffab15caddc7
Marcin Szawara <szawara.marcin@gmail.com>
2023-09-17 17:33:21 +0100

    choose and change language functions

--- javascript/script.js
+++ javascript/script.js
@@ -1,6 +1,35 @@
 const plBtn = document.querySelector(".language__polish");
-const plEng = document.querySelector(".language__english");
+const engBtn = document.querySelector(".language__english");
 const gramsInput = document.querySelector(".aplication__grams");
 const firstProduct = document.querySelector(".aplication__first--product");
-const secondProduct = document.querySelector(".aplication__second--product");
+const firstOption = document.querySelector(".aplication__first--product option");
+const secondProduct = document.querySelector(".aplication__first--product");
+const secondOption = document.querySelector(".aplication__second--product option");
 const result = document.querySelector(".aplication__number");
+const text = document.querySelector(".aplication__text");
+// WPROWADZENI POLSKICH NAZW________________________
+const polishLanguage = () => {
+    plBtn.classList.add("button--active");
+    text.textContent = "gramów";
+    gramsInput.placeholder = "wpisz gramature";
+    firstOption.textContent = "wybierz produkt, który chcesz zmienić";
+    secondOption.textContent = "wybierz produkt, który chcesz zjeść";
+};
+// WPROWADZANIE ANGIELSKICH NAZW________________________
+const englishLanguage = () => {
+    engBtn.classList.add("button--active");
+    text.textContent = "grams";
+    gramsInput.placeholder = "enter grammage";
+    firstOption.textContent = "select the product, which you want to change";
+    secondOption.textContent = "select the product, which you want to eat";
+};
+// SPRAWDANIE JEZYKA________________________
+(document.documentElement.lang === "pl") ? polishLanguage() : englishLanguage();
+plBtn.addEventListener("click", () => {
+    engBtn.classList.remove("button--active");
+    polishLanguage();
+});
+engBtn.addEventListener("click", () => {
+    plBtn.classList.remove("button--active");
+    englishLanguage();
+});
--- typescript/script.ts
+++ typescript/script.ts
@@ -1,6 +1,43 @@
 const plBtn: HTMLButtonElement = document.querySelector(".language__polish");
-const plEng: HTMLButtonElement = document.querySelector(".language__english");
+const engBtn: HTMLButtonElement = document.querySelector(".language__english");
 const gramsInput: HTMLInputElement = document.querySelector(".aplication__grams");
 const firstProduct: HTMLInputElement = document.querySelector(".aplication__first--product");
-const secondProduct: HTMLInputElement = document.querySelector(".aplication__second--product");
-const result: HTMLDivElement = document.querySelector(".aplication__number")
+const firstOption: HTMLInputElement = document.querySelector(".aplication__first--product option");
+const secondProduct: HTMLInputElement = document.querySelector(".aplication__first--product");
+const secondOption: HTMLInputElement = document.querySelector(".aplication__second--product option");
+const result: HTMLDivElement = document.querySelector(".aplication__number");
+const text: HTMLDivElement = document.querySelector(".aplication__text");
+
+// WPROWADZENI POLSKICH NAZW________________________
+const polishLanguage = () => {
+    plBtn.classList.add("button--active");
+    text.textContent = "gramów";
+    gramsInput.placeholder = "wpisz gramature";
+    firstOption.textContent = "wybierz produkt, który chcesz zmienić";
+    secondOption.textContent = "wybierz produkt, który chcesz zjeść";
+};
+
+// WPROWADZANIE ANGIELSKICH NAZW________________________
+const englishLanguage = () => {
+    engBtn.classList.add("button--active");
+    text.textContent = "grams";
+    gramsInput.placeholder = "enter grammage";
+    firstOption.textContent = "select the product, which you want to change";
+    secondOption.textContent = "select the product, which you want to eat";
+};
+
+// SPRAWDANIE JEZYKA________________________
+(document.documentElement.lang === "pl")? polishLanguage() : englishLanguage();
+
+plBtn.addEventListener("click", ()=> {
+    engBtn.classList.remove("button--active");
+    polishLanguage();
+})
+
+engBtn.addEventListener("click", ()=> {
+    plBtn.classList.remove("button--active");
+    englishLanguage();
+})
+
+
+
